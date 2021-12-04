2b6c5e6307c50e4d4a3a4ddd5a554e679c9bef36
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-04 11:10:08 +0300

    Добавлены метаданные по жанрам и персоналиям

--- src/api/v1/person.py
+++ src/api/v1/person.py
@@ -1,9 +1,8 @@
 from http import HTTPStatus
-from typing import List
-
-from fastapi import APIRouter, Depends, HTTPException, Query
+from typing import List, Optional
 
 from api.v1.film import generate_body
+from fastapi import APIRouter, Depends, HTTPException, Query
 from models.person_response import PersonFilmResponse, PersonResponse
 from services.person import PersonService, get_person_service
 from strings.exceptions import PERSON_NOT_FOUND
@@ -11,35 +10,65 @@ from strings.exceptions import PERSON_NOT_FOUND
 router = APIRouter()
 
 
-@router.get("/search", response_model=List[PersonResponse])
+@router.get("/search", response_model=List[PersonResponse],
+            summary="Поиск по персоналиям в кино",
+            description="Полнотекстовый поиск по персоналиям, участвовавшим в создании кино",
+            response_description="id и имя человека, а также список фильмов в которых он участвовал",
+            tags=['полнотекстовый поиск', 'персоналии'],
+            )
 async def person_search(
-    query: str,
-    page_number: int = Query(None, alias="page[number]"),
-    page_size: int = Query(None, alias="page[size]"),
-    service: PersonService = Depends(get_person_service),
+        query: str = Query(
+            ...,
+            title="запрос",
+            description="Поисковый запрос",
+        ),
+        page_number: Optional[int] = Query(
+            None, alias="page[number]",
+            title="страница",
+            description="Порядковый номер страницы результатов",
+        ),
+        page_size: Optional[int] = Query(
+            None, alias="page[size]",
+            title="размер страницы",
+            description="Количество документов на странице",
+        ),
+        service: PersonService = Depends(get_person_service),
 ) -> List[PersonResponse]:
     body = await generate_body(query, page_number, page_size)
     searched_persons = await service.search(body=body)
     if not searched_persons:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=PERSON_NOT_FOUND)
     return [
-        PersonResponse(uuid=person.id, full_name=person.fullname, films=[{film.id: film.role} for film in person.film_ids])
+        PersonResponse(uuid=person.id, full_name=person.fullname,
+                       films=[{film.id: film.role} for film in person.film_ids])
         for person in searched_persons
     ]
 
 
-@router.get("/{person_id}", response_model=PersonResponse)
-async def person_details(person_id: str, person_service: PersonService = Depends(get_person_service)) -> PersonResponse:
+@router.get("/{person_id}", response_model=PersonResponse,
+            summary="Информация по конкретному человеку",
+            description="Полная информация по одному конкретному человеку",
+            response_description="id и имя человека, а также список фильмов в которых он участвовал",
+            tags=['подробный вывод', 'персоналии'],
+            )
+async def person_details(person_id: str = Query(...,
+                                                title="id персоны",
+                                                description="Id персоны по которому будет выведена полная информация",
+                                                ),
+                         person_service: PersonService = Depends(get_person_service)) -> PersonResponse:
     person = await person_service.get_by_id(person_id)
     if not person:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=PERSON_NOT_FOUND)
-    return PersonResponse(uuid=person.id, full_name=person.fullname, films=[{film.id: film.role} for film in person.film_ids])
+    return PersonResponse(uuid=person.id, full_name=person.fullname,
+                          films=[{film.id: film.role} for film in person.film_ids])
 
 
 @router.get("/{person_id}/film", response_model=List[PersonFilmResponse])
-async def person_films(person_id: str, person_service: PersonService = Depends(get_person_service)) -> List[PersonFilmResponse]:
+async def person_films(person_id: str, person_service: PersonService = Depends(get_person_service)) -> List[
+    PersonFilmResponse]:
     person = await person_service.get_by_id(person_id)
     if not person:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=PERSON_NOT_FOUND)
 
-    return [PersonFilmResponse(uuid=film.id, title=film.title, imdb_rating=film.imdb_rating) for film in person.film_ids]
+    return [PersonFilmResponse(uuid=film.id, title=film.title, imdb_rating=film.imdb_rating) for film in
+            person.film_ids]
