26c582d2c832e0629516cbbbd0e20e44d7784407
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-05 02:45:38 +0300

    Базовая конфигурация для тестов

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,15 +1,23 @@
+import asyncio
+from dataclasses import dataclass
+
 import aiohttp
 import aioredis
 import pytest
-
-from dataclasses import dataclass
-from multidict import CIMultiDictProxy
-from elasticsearch import AsyncElasticsearch
-
 from functional import settings
-
+from functional.utils.elastic_wrapper import ElasticWrapper
+from multidict import CIMultiDictProxy
 
 SERVICE_URL = 'http://127.0.0.1:8000'
+indexes_dict = {'movies': '../testdata/film_index_settings.json',
+                'genre': '../testdata/genre_index_settings.json',
+                'person': '../testdata/person_index_settings.json',
+                }
+
+data_dict = {'movies': '../testdata/films_data.json',
+             'genre': '../testdata/genres_data.json',
+             'person': '../testdata/persons_data.json',
+             }
 
 
 @dataclass
@@ -19,10 +27,30 @@ class HTTPResponse:
     status: int
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
 @pytest.fixture(scope='session')
 async def es_client():
-    client = AsyncElasticsearch(hosts=f"{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}")
+    client = ElasticWrapper(hosts=f"{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}")
+
+    for index_name, index_path in indexes_dict.items():
+        await client.create_index_from_file(index_name=index_name,
+                                            index_settings_path=index_path)
+
+    for index_name, data_path in data_dict.items():
+        await client.load_from_file(index_name=index_name, data_path=data_path)
+
     yield client
+
+    for index_name in indexes_dict.keys():
+        await client.delete_data(index_name=index_name)
+        await client.delete_index(index_name=index_name)
+
     await client.close()
 
 
@@ -51,5 +79,4 @@ def make_get_request(session):
                 headers=response.headers,
                 status=response.status,
             )
-
     return inner
