1ea48a911161f38fd7f939065db1f9820ad9c0cd
UtkinVadim <utkinvadim1991@gmail.com>
2021-12-05 14:26:41 +0300

    Изменил создание индексов без использования модели данных.

--- tests/functional/src/test_genres.py
+++ tests/functional/src/test_genres.py
@@ -1,72 +1,46 @@
+import json
 import pytest
-
-from models.genre import Genre
 
 
 @pytest.mark.asyncio
 async def test_all_genres(make_get_request):
-    response = await make_get_request(method='/genre/')
+    response = await make_get_request(method="/genre/")
     assert response.status == 200, response.body
     assert len(response.body) == 3, response.body
-    expected = [
-        {
-            'uuid': '1',
-            'name': 'Action'
-        },
-        {
-            'uuid': '2',
-            'name': 'Comedy'
-        },
-        {
-            'uuid': '3',
-            'name': 'AbraCadabra'
-        }
-    ]
+    expected = [{"uuid": "1", "name": "Action"}, {"uuid": "2", "name": "Comedy"}, {"uuid": "3", "name": "AbraCadabra"}]
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_find_specific_genre(make_get_request):
-    response = await make_get_request(method='/genre/1')
+    response = await make_get_request(method="/genre/1")
 
     assert response.status == 200
 
-    expected = {
-            'uuid': '1',
-            'name': 'Action'
-        }
+    expected = {"uuid": "1", "name": "Action"}
 
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_genre_no_found(make_get_request):
-    response = await make_get_request(method='/genre/fake_genre')
+    response = await make_get_request(method="/genre/fake_genre")
 
     assert response.status == 404
 
-    expected = {
-        'detail': {
-            '_index': 'genre',
-            '_type': '_doc',
-            '_id': 'fake_genre',
-            'found': False
-        }
-    }
+    expected = {"detail": {"_index": "genre", "_type": "_doc", "_id": "fake_genre", "found": False}}
 
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_find_from_redis_cache(make_get_request, redis_client):
-    await redis_client.set("genre::genre_4", Genre(id="genre_4", name="Genre from cache").json())
-    response = await make_get_request(method='/genre/genre_4')
+    genre = json.dumps({"id": "genre_4", "name": "Genre from cache"})
+    await redis_client.set("genre::genre_4", genre)
+    response = await make_get_request(method="/genre/genre_4")
 
     assert response.status == 200
 
-    expected = {
-        'uuid': 'genre_4',
-        'name': 'Genre from cache'
-    }
+    expected = {"uuid": "genre_4", "name": "Genre from cache"}
 
     assert response.body == expected
--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -1,41 +1,16 @@
+import json
 import pytest
-
-from models.person import Person, Film
 
 
 @pytest.mark.asyncio
 async def test_all_persons(make_get_request):
-    response = await make_get_request(method='/person/search', params={"query": "Person"})
+    response = await make_get_request(method="/person/search", params={"query": "Person"})
     assert response.status == 200, response.body
     assert len(response.body) == 3, response.body
     expected = [
-        {
-            'uuid': '1',
-            'full_name': 'Person 1',
-            'films': [
-                {
-                    '1': 'role 1'
-                }
-            ]
-        },
-        {
-            'uuid': '2',
-            'full_name': 'Person 2',
-            'films': [
-                {
-                    '2': 'role 2'
-                }
-            ]
-        },
-        {
-            'uuid': '3',
-            'full_name': 'Person 3',
-            'films': [
-                {
-                    '3': 'role 3'
-                }
-            ]
-        }
+        {"uuid": "1", "full_name": "Person 1", "films": [{"1": "role 1"}]},
+        {"uuid": "2", "full_name": "Person 2", "films": [{"2": "role 2"}]},
+        {"uuid": "3", "full_name": "Person 3", "films": [{"3": "role 3"}]},
     ]
     assert response.body == expected
 
@@ -44,62 +19,46 @@ async def test_all_persons(make_get_request):
 async def test_person_not_found(make_get_request):
     response = await make_get_request(method="/person/search", params={"query": "good_modern_russian_actor"})
     assert response.status == 404, response.body
-    expected = {
-        'detail': 'person not found'
-    }
+    expected = {"detail": "person not found"}
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_find_specific_person(make_get_request):
-    response = await make_get_request(method='/person/1')
+    response = await make_get_request(method="/person/1")
     assert response.status == 200, response.body
-    expected = {
-        'uuid': '1',
-        'full_name': 'Person 1',
-        'films': [
-            {
-                '1': 'role 1'
-            }
-        ]
-    }
+    expected = {"uuid": "1", "full_name": "Person 1", "films": [{"1": "role 1"}]}
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_find_person_films(make_get_request):
-    response = await make_get_request(method='/person/1/film')
+    response = await make_get_request(method="/person/1/film")
     assert response.status == 200, response.body
-    expected = [
-        {
-            'uuid': '1',
-            'title': 'film 1',
-            'imdb_rating': 1.0
-        }
-    ]
+    expected = [{"uuid": "1", "title": "film 1", "imdb_rating": 1.0}]
     assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_find_person_from_redis_cache(make_get_request, redis_client):
     personfilms = [
-        Film(id="1", title="Хоббит: Нежданное путешествие", imdb_rating=9, role="Хоббит"),
-        Film(id="2", title="Хоббит: Пустошь Смауга", imdb_rating=8, role="Хоббит"),
-        Film(id="3", title="Хоббит: Битва пяти воинств", imdb_rating=10, role="Хоббит")
+        {"id": "1", "title": "Хоббит: Нежданное путешествие", "imdb_rating": 9, "role": "Хоббит"},
+        {"id": "2", "title": "Хоббит: Пустошь Смауга", "imdb_rating": 8, "role": "Хоббит"},
+        {"id": "3", "title": "Хоббит: Битва пяти воинств", "imdb_rating": 10, "role": "Хоббит"}
     ]
-    person = Person(id="hobbit_id", fullname="Bilbo Baggins", film_ids=personfilms)
-    await redis_client.set(f"person::{person.id}", person.json())
-    response = await make_get_request(method=f'/person/{person.id}')
+    person = json.dumps({"id": "hobbit_id", "fullname": "Bilbo Baggins", "film_ids": personfilms})
+    await redis_client.set("person::hobbit_id", person)
+    response = await make_get_request(method="/person/hobbit_id")
 
     assert response.status == 200
 
     expected = {
-        'uuid': 'hobbit_id',
-        'full_name': 'Bilbo Baggins',
-        'films': [
-            {'1': 'Хоббит'},
-            {'2': 'Хоббит'},
-            {'3': 'Хоббит'}
+        "uuid": "hobbit_id",
+        "full_name": "Bilbo Baggins",
+        "films": [
+            {"1": "Хоббит"},
+            {"2": "Хоббит"},
+            {"3": "Хоббит"}
         ]
     }
 
@@ -110,12 +69,5 @@ async def test_find_person_from_redis_cache(make_get_request, redis_client):
 async def test_person_not_found_in_elastic(make_get_request):
     response = await make_get_request(method="/person/good_modern_russian_actor")
     assert response.status == 404, response.body
-    expected = {
-        'detail': {
-            '_index': 'person',
-            '_type': '_doc',
-            '_id': 'good_modern_russian_actor',
-            'found': False
-        }
-    }
+    expected = {"detail": {"_index": "person", "_type": "_doc", "_id": "good_modern_russian_actor", "found": False}}
     assert response.body == expected
