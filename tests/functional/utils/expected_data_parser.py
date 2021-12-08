9b351c522c0f3721eb98d31d2bd9d08001d81a9a
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-08 12:36:56 +0300

    Класс по добыче ожидаемых данных из json файла разбит на три разных, отдельно для жанров, персон и фильмов.

--- tests/functional/utils/expected_data_parser.py
+++ tests/functional/utils/expected_data_parser.py
@@ -1,15 +1,15 @@
-from pprint import pprint
 from typing import Union
 
 from functional.utils.data_parser import DataParser
 
 
-class ExpectedDataParser:
+class ExpectedGenre:
     def __init__(self):
         self.parser = DataParser()
+        self.file_data_path = "genres_data.json"
 
     async def get_expected_genres_data(self, genre_id: str = None) -> Union[list, dict, str]:
-        genres_data_from_file = await self.parser.get_data_from_file(file_name="genres_data.json")
+        genres_data_from_file = await self.parser.get_data_from_file(file_name=self.file_data_path)
         expected_data = [
             {"uuid": genres.get("id"), "name": genres.get("name")}
             for genres in map(lambda genre_data: genre_data.get("_source"), genres_data_from_file)
@@ -22,8 +22,18 @@ class ExpectedDataParser:
                 return f"В файле genres_data.json не найдена запись с id {genre_id}"
         return expected_data
 
+
+class ExpectedPerson:
+    def __init__(self):
+        self.parser = DataParser()
+        self.file_data_path = "persons_data.json"
+
+    @staticmethod
+    async def get_person_roles(film_ids: list) -> list:
+        return [{film["id"]: film["role"]} for film in film_ids]
+
     async def get_expected_persons_data(self, person_id: str = None):
-        persons_data_from_file = await self.parser.get_data_from_file(file_name="persons_data.json")
+        persons_data_from_file = await self.parser.get_data_from_file(file_name=self.file_data_path)
         expected_data = [
             {"uuid": person.get("id"),
              "full_name": person.get("fullname"),
@@ -38,12 +48,8 @@ class ExpectedDataParser:
                 return f"В файле persons_data.json не найдена запись с id {person_id}"
         return expected_data
 
-    @staticmethod
-    async def get_person_roles(film_ids: list) -> list:
-        return [{film["id"]: film["role"]} for film in film_ids]
-
     async def get_expected_person_film_data(self, person_id: str):
-        persons_data_from_file = await self.parser.get_data_from_file(file_name="persons_data.json")
+        persons_data_from_file = await self.parser.get_data_from_file(file_name=self.file_data_path)
         person_data = list(filter(lambda person: person["_id"] == person_id, persons_data_from_file))[0]
         expected_data = [
             {"uuid": film_info.get("id"),
@@ -53,33 +59,43 @@ class ExpectedDataParser:
         ]
         return expected_data
 
+
+class ExpectedFilm:
+    def __init__(self):
+        self.parser = DataParser()
+        self.file_data_path = "films_data.json"
+
     async def get_film_detailed_data(self, film_id: str):
-        films_data_from_file = await self.parser.get_data_from_file(file_name="films_data.json")
+        films_data_from_file = await self.parser.get_data_from_file(file_name=self.file_data_path)
         film_data = list(filter(lambda film: film["_id"] == film_id, films_data_from_file))[0]["_source"]
+        print(film_data)
         film_data.pop("actors_names")
         film_data.pop("writers_names")
         return film_data
 
     async def get_film_data(self, genre_id: str = None, page_size: int = None, sort_by: str = None):
-        films_data_from_file = await self.parser.get_data_from_file(file_name="films_data.json")
+        films_data_from_file = await self.parser.get_data_from_file(file_name=self.file_data_path)
         films_data = [film_data["_source"] for film_data in films_data_from_file]
         if genre_id:
-            genre = await self.get_expected_genres_data(genre_id)
+            genre_parser = ExpectedGenre()
+            genre = await genre_parser.get_expected_genres_data(genre_id)
             films_data = list(filter(
                 lambda film_data: film_data if genre["name"] in film_data["genre"] else None, films_data)
             )
+
         films_data = [
             {
                 "id": film["id"], "title": film["title"], "imdb_rating": film["imdb_rating"]
             }
             for film in films_data
         ]
-        if page_size:
-            films_data = films_data[:page_size]
+
         if sort_by:
             reverse = False
             if "-" in sort_by:
                 sort_by = sort_by[1:]
                 reverse = True
             films_data = sorted(films_data, key=lambda film: film[sort_by], reverse=reverse)
+        if page_size:
+            films_data = films_data[:page_size]
         return films_data
