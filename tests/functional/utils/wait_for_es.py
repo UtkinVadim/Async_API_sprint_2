eb153a42210f8844cbb532cbb994c8002147365e
UtkinVadim <utkinvadim1991@gmail.com>
2021-12-09 01:28:35 +0300

    Добавил backoff дял подключения к сервисам в тестах

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
 import sys
-import time
 
+import backoff
 import elasticsearch
-from settings import ELASTIC_HOST, ELASTIC_PORT
+from functional.settings import ELASTIC_HOST, ELASTIC_PORT
 
 logger = logging.getLogger(__name__)
 log_format = "%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s"
@@ -14,19 +14,19 @@ bold_green = "\x1b[32;1m"
 reset = "\x1b[0m"
 
 
+def backoff_handler(_details):
+    logger.exception("Waiting for elastic...", exc_info=False)
+
+
+@backoff.on_exception(backoff.expo, ConnectionError, max_time=300, max_tries=15, on_backoff=backoff_handler)
 async def main():
-    elastic_online = False
-    while not elastic_online:
-        time.sleep(2)
-        try:
-            elastic = elasticsearch.AsyncElasticsearch(hosts=[f"{ELASTIC_HOST}:{ELASTIC_PORT}"])
-            elastic_answer = await elastic.ping()
-            if elastic_answer:
-                elastic_online = True
-                logger.info("Elastic reporting!".join([bold_green, reset]))
-                await elastic.close()
-        except ConnectionRefusedError:
-            logger.exception("Waiting for elastic...", exc_info=False)
+    elastic = elasticsearch.AsyncElasticsearch(hosts=[f"{ELASTIC_HOST}:{ELASTIC_PORT}"])
+    elastic_answer = await elastic.ping(error_trace=False)
+    if elastic_answer:
+        logger.info("Elastic reporting!".join([bold_green, reset]))
+        await elastic.close()
+    else:
+        raise ConnectionError
 
 
 if __name__ == "__main__":
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
 import sys
-import time
 
 import aioredis
-from settings import REDIS_HOST, REDIS_PORT
+import backoff
+from functional.settings import REDIS_HOST, REDIS_PORT
 
 logger = logging.getLogger(__name__)
 log_format = "%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s"
@@ -14,20 +14,18 @@ bold_green = "\x1b[32;1m"
 reset = "\x1b[0m"
 
 
+def backoff_handler(_details):
+    logger.exception("Waiting for redis...", exc_info=False)
+
+
+@backoff.on_exception(backoff.expo, ConnectionRefusedError, max_time=300, max_tries=15, on_backoff=backoff_handler)
 async def main():
-    redis_online = False
-    while not redis_online:
-        time.sleep(2)
-        try:
-            redis = await aioredis.create_redis((REDIS_HOST, REDIS_PORT))
-            redis_answer = await redis.ping()
-            if redis_answer == b"PONG":
-                redis_online = True
-                logger.info("Redis reporting!".join([bold_green, reset]))
-                redis.close()
-                await redis.wait_closed()
-        except ConnectionRefusedError:
-            logger.exception("Waiting for redis...", exc_info=False)
+    redis = await aioredis.create_redis((REDIS_HOST, REDIS_PORT))
+    redis_answer = await redis.ping()
+    if redis_answer == b"PONG":
+        logger.info("Redis reporting!".join([bold_green, reset]))
+        redis.close()
+        await redis.wait_closed()
 
 
 if __name__ == "__main__":
