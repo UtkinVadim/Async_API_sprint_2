7b0b28693aad89ee98c75f5a7862d125eecc5005
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-08 18:26:33 +0300

    Метод get_film_data упрощён. Из него выделены вспомогательные методы по фильрации и сортировке

--- tests/functional/utils/expected_data_parser.py
+++ tests/functional/utils/expected_data_parser.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 
 from functional.utils.data_parser import DataParser
 
@@ -44,16 +44,7 @@ class ExpectedPerson:
         ]
 
         if query:
-            result_list = []
-            for person in expected_data:
-                for field, field_value in person.items():
-                    if field not in ["full_name"]:
-                        continue
-                    if query.lower() in str(field_value).lower():
-                        if person not in result_list:
-                            result_list.append(person)
-                        continue
-            expected_data = result_list
+            expected_data = await self.filter_persons_by_query(query, expected_data)
 
         if person_id:
             for person_expected_data in expected_data:
@@ -71,6 +62,19 @@ class ExpectedPerson:
             for film_info in person_data["_source"]["film_ids"]
         ]
         return expected_data
+
+    @staticmethod
+    async def filter_persons_by_query(query: str, persons):
+        result_list = []
+        for person in persons:
+            for field, field_value in person.items():
+                if field not in ["full_name"]:
+                    continue
+                if query.lower() in str(field_value).lower():
+                    if person not in result_list:
+                        result_list.append(person)
+                    continue
+        return result_list
 
 
 class ExpectedFilm:
@@ -97,38 +101,52 @@ class ExpectedFilm:
         films_data = [film_data["_source"] for film_data in films_data_from_file]
 
         if query:
-            result_list = []
-            for film in films_data:
-                for field, field_value in film.items():
-                    if field not in ["title", "description"]:
-                        continue
-                    if query.lower() in str(field_value).lower():
-                        if film not in result_list:
-                            result_list.append(film)
-                        continue
-
-            films_data = result_list
+            films_data = await self.filter_films_by_query(query, films_data)
 
         if genre_id:
-            genre_parser = ExpectedGenre()
-            genre = await genre_parser.get_expected_genres_data(genre_id)
-            films_data = list(
-                filter(lambda film_data: film_data if genre["name"] in film_data["genre"] else None, films_data)
-            )
+            films_data = await self.filter_films_by_genre_id(genre_id, films_data)
 
         films_data = [
             {"id": film["id"], "title": film["title"], "imdb_rating": film["imdb_rating"]} for film in films_data
         ]
 
         if sort_by:
-            reverse = False
-            if "-" in sort_by:
-                sort_by = sort_by[1:]
-                reverse = True
-            films_data = sorted(films_data, key=lambda film: film[sort_by], reverse=reverse)
+            films_data = await self.sort_films(sort_by, films_data)
+
         if page_number and page_size:
             films_data = films_data[(page_number - 1) * page_size : page_number * page_size]
         elif page_size:
             films_data = films_data[:page_size]
 
+        return films_data
+
+    @staticmethod
+    async def filter_films_by_query(query: str, films_data: List):
+        result_list = []
+        for film in films_data:
+            for field, field_value in film.items():
+                if field not in ["title", "description"]:
+                    continue
+                if query.lower() in str(field_value).lower():
+                    if film not in result_list:
+                        result_list.append(film)
+                    continue
+        return result_list
+
+    @staticmethod
+    async def filter_films_by_genre_id(genre_id: str, films_data: List):
+        genre_parser = ExpectedGenre()
+        genre = await genre_parser.get_expected_genres_data(genre_id)
+        films_data = list(
+            filter(lambda film_data: film_data if genre["name"] in film_data["genre"] else None, films_data)
+        )
+        return films_data
+
+    @staticmethod
+    async def sort_films(sort_by: str, films_data):
+        reverse = False
+        if "-" in sort_by:
+            sort_by = sort_by[1:]
+            reverse = True
+        films_data = sorted(films_data, key=lambda film: film[sort_by], reverse=reverse)
         return films_data
